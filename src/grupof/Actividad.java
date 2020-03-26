94ac54498998c74797006aba827c9c23b48052c8
CARLOS <CARLOS@MSI>
2020-03-26 11:31:27 +0100

    Primer commit

--- src/grupof/Actividad.java
+++ src/grupof/Actividad.java
@@ -5,19 +5,9 @@
  */
 package grupof;
 
-import java.io.Serializable;
-import java.util.Date;
-import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.OneToMany;
-import javax.persistence.Temporal;
-
+import java.io.*;
+import java.util.*;
+import javax.persistence.*;
 /**
  * SII
  * 3ºA Ingeniería Informática 
